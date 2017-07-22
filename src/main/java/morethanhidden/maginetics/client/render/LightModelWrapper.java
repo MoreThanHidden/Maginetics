82a96a805639fc395ba3a7b61594f1759bbb1bb9
John Hannan <john816_@hotmail.com>
2017-07-23 01:00:48 +0800

    Cleanup of a test interface

--- src/main/java/morethanhidden/maginetics/client/render/LightModelWrapper.java
+++ src/main/java/morethanhidden/maginetics/client/render/LightModelWrapper.java
@@ -1,17 +1,17 @@
 package morethanhidden.maginetics.client.render;
 
-import morethanhidden.maginetics.IModelGlow;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.common.model.IModelState;
 
 import java.util.function.Function;
 
-public class LightModelWrapper implements IModelGlow {
+public class LightModelWrapper implements IModel {
 
     private final ResourceLocation base;
     private final ResourceLocation overlay;
